b0fe740696146dd7fbed851a5fc7eb822289add9
AttilaBalla <xattus@gmail.com>
2018-04-24 22:34:40 +0200

    basic api controller added

--- src/main/java/com/usermanager/controller/ApiController.java
+++ src/main/java/com/usermanager/controller/ApiController.java
@@ -1,7 +1,15 @@
 package com.usermanager.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ApiController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String renderIndex() {
+
+        return "index";
+    }
 }
